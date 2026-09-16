971945b2bcc93ab70bce3814455edcacac6d73e9
akshaya1024 <akshusiva24@gmail.com>
2026-09-16 19:51:53 +0530

    Update delete account page title

--- signup.js
+++ signup.js
@@ -1,24 +1,4 @@
-// let verifyBtn = document.getElementById("verifyBtn");
-// let mobileNumber = document.getElementById("mobileNumber");
-// let verifyPopup = document.getElementById("verifyPopup");
 
-// verifyBtn.onclick = function () {
-
-//     let number = mobileNumber.value.trim();
-
-//     if (!/^[0-9]{10}$/.test(number)) {
-
-//         alert("Enter a valid number");
-
-//         return;
-//     }
-
-//     verifyPopup.style.display = "block";
-
-//     setTimeout(function () {
-//         verifyPopup.style.display = "none";
-//     }, 2500);
-// };
 
 let verifyBtn = document.getElementById("verifyBtn");
 let mobileNumber = document.getElementById("mobileNumber");
@@ -39,7 +19,7 @@ verifyBtn.onclick = function () {
 
     verifyPopup.style.display = "block";
     localStorage.setItem("loginSuccess", "true");
-    localStorage.setItem("mobileNumber", number);
+    // localStorage.setItem("mobileNumber", number);
     setTimeout(function () {
         window.location.href ="index.html"
     }, 1500);
